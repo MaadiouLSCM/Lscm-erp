de323879828cc2626be058a7e6f6b28fa3509488
MaadiouLSCM <maadiou@gmail.com>
2026-03-06 23:30:17 +0100

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,1 +1,14 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+    console.log('✅ Connected to PostgreSQL via Prisma');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
